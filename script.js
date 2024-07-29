7c6af129af5146b9ae94053094d4ab38f4cfcc65
Kemo Hashem <138363179+KemoWebZoon@users.noreply.github.com>
2024-07-29 17:03:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const button = document.getElementById("get-location-button");
+
+async function getData(lat, long) {
+  try {
+    alert(`latitude: ${lat}, longitude: ${long}`);
+    return {
+      latitude: lat,
+      longitude: long,
+    };
+  } catch (error) {
+    console.error("There has been a problem:", error);
+  }
+}
+
+async function gotLocation(position) {
+  const result = await getData(
+    position.coords.latitude,
+    position.coords.longitude
+  );
+  console.log("Latitude:", result.latitude);
+  console.log("Longitude:", result.longitude);
+}
+
+function failedToGet() {
+  console.log("Failed To Get Location");
+}
+
+button.addEventListener("click", async () => {
+  navigator.geolocation.getCurrentPosition(gotLocation, failedToGet);
+});
